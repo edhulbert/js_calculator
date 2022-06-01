31c2d3ce1ad38cd08d6767699d6dd80e91a4801b
Ed Hulbert <edward.hulbert@hotmail.co.uk>
2022-06-01 09:21:56 +0100

    testing

--- calculator_testing_start/calculator.js
+++ calculator_testing_start/calculator.js
@@ -1,18 +1,30 @@
-const sum = function(a, b){
-    return a + b;
+"use strict";
+
+const BigNumber = require("bignumber.js");
+
+
+const sum = (a,b) => a + b;
+
+const subtract = (a,b) => a - b;
+
+const multiply = (a,b) => a*b;
+
+const divide = (a,b) => a/b;
+
+const modulus = (a,b) => {
+    
+    return BigNumber(a)%BigNumber(b);
+}
+
+const even = (a) => {
+    if (modulus(BigNumber(a),2) == 0) return true;
+    else return false;
 };
 
-const subtract = null;
-
-const multiply = null;
-
-const divide = null;
-
-const modulus = null;
-
-const even = null;
-
-const odd = null;
+const odd = (a) => {
+    if (modulus(a,2) != 0) return true;
+    else return false;
+};
 
 module.exports = { 
     sum, 
--- calculator_testing_start/calculator.test.js
+++ calculator_testing_start/calculator.test.js
@@ -8,6 +8,8 @@ const {
   odd,
 } = require("./calculator");
 
+const BigNumber = require("bignumber.js");
+
 describe('sum', () => { 
   
   test('can add two small positive numbers', () => {
@@ -16,22 +18,53 @@ describe('sum', () => {
     expect(actual).toBe(expected);
   });
 
-  test.skip('can add two large positive numbers', () => { 
-    
+  test('can add two large positive numbers', () => { 
+    actual = sum(231441231125123124113231, 15231251512312412251235423);
+    //expected = 15462692743437535375348654; <= this is the full answer, but js doesnt seem to store to this precision
+    //expected = 15462692743437530000000000; <= if you change the last 5 to a 0, the test fails
+    //expected = 15462692743437535900000000; <= changing numbers after the last five changes nothing, including decimals  
+    expected = 15462692743437535000000000; // <= this is the exact level of precision that js recognises as correct. stored as 1.5462692743437535e+25
+    expect(actual).toBe(expected);
   });
 
-  test.skip('can add two negative numbers', () => {
-    
+  test('can add two negative numbers', () => {
+    actual = sum(-5, -6);
+    expected = -11;
+    expect(actual).toBe(expected);
   });
 
-  test.skip('can add zero', () => {
-    
+  test('can add zero', () => {
+    actual = sum(5,0);
+    expected = 5;
+    expect(actual).toBe(expected);
   });
 
 });
 
 describe('subtract', () => {
+  test("can subtract smaller number from larger number", () => {
+    actual = subtract(10,5);
+    expected = 5;
+    expect(actual).toBe(expected);
+  });
 
+  test("can subtract larger number from smaller number", () => {
+    actual = subtract(5,10);
+    expected = -5;
+    expect(actual).toBe(expected);
+  });
+
+  test("can subtract decimals", () => {
+    actual = subtract(5.4213, 12.2);
+    expected = -6.7787;
+    expect(actual).toBe(expected);
+  });
+
+  test("can subtract zero", () => {
+    actual = subtract(5.1231,0);
+    expected = 5.1231;
+    expect(actual).toBe(expected);
+  });
 });
 
 describe('multiply', () => {
@@ -43,11 +76,80 @@ describe('divide', () => {
 });
 
 describe('modulus', () => {
+  test("can return 1 for 3 and 2", () => {
+    actual = modulus(3,2);
+    expected = 1;
+    expect(actual).toBe(expected);
+  });
 
+  test("can return 5 for 5 and 111", () => {
+    actual = modulus(5,111);
+    expected = 5;
+    expect(actual).toBe(expected);
+  });
+
+  test("can return modulus for very large number and small number", () => {
+    actual = modulus(21314123123123135512n,2);
+    expected = 0;
+    expect(actual).toBe(expected);
+  });
+
+  test("can return modulus for very large number and small number", () => {
+    actual = modulus(21314123123123135511n,2);
+    expected = 1;
+    expect(actual).toBe(expected);
+  });
 });
 
 describe('even', () => {
+  test("can return true for even number", () => {
+    actual = even(6);
+    expected = true;
+    expect(actual).toBe(expected);
+  });
 
+  test("can return false for odd number", () => {
+    actual = even(5);
+    expected = false;
+    expect(actual).toBe(expected);
+  });
+
+  test("can return false for decimal", () => {
+    actual = even(6.1);
+    expected = false;
+    expect(actual).toBe(expected);
+  });
+
+  test("can return true for zero", () => {
+    actual = even(0);
+    expected = true;
+    expect(actual).toBe(expected);
+  });
+
+  test("can return true for negative even number", () => {
+    actual = even(-6);
+    expected = true;
+    expect(actual).toBe(expected);
+  });
+
+  test("can return false for negative odd number", () => {
+    actual = even(-11);
+    expected = false;
+    expect(actual).toBe(expected);
+  });
+
+  test("can return true for very large even number", () => {
+    actual = even(11111111111111111111111111111112n);
+    expected = true;
+    expect(actual).toBe(expected);
+  });
+
+  test("can return false for very large odd number", () => {
+    actual = even(11111111111111111111111111111111n);
+    expected = false;
+    expect(actual).toBe(expected);
+    // initially failed, why? 
+  });
 });
 
 describe('odd', () => {
